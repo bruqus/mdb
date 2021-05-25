6b4b55d3eb5006d199f9057fc63d00a5ca2e0846
Nikita Bruqus <bruqusg@gmail.com>
2021-05-25 15:35:05 +0300

    feat: add showMovie handler

---
+++ cmd/api/movie.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/bruqus/mdb/internal/data"
+)
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Foo baz",
+		Year:      1999,
+		Runtime:   102,
+		Genres:    []string{"drama", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+}
